b11ad9da9c46786e3d5c12c82d5762afef264d51
Naveen V <velnaveen99@gmail.com>
2022-03-14 14:46:17 +0530

    import types from types-terra

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,19 +1,19 @@
 import {Transfer, Rewards} from "../types";
-import { TerraContractEvent, TerraContractCall } from '@subql/contract-processors/dist/terra';
+import { TerraEvent, TerraCall } from '@subql/types-terra';
 import * as crypto from "crypto";
 
-export async function handleFrontierEvmEvent(event: TerraContractEvent): Promise<void> {
+export async function handleEvent(event: TerraEvent): Promise<void> {
     for(var i = 0; i < event.event["transfer"]["sender"].length; i++) {
         const idx = crypto.randomBytes(32).toString("hex");
         const transfer = new Transfer(`${event.transactionHash}-${event.msgIndex}-${idx}`);
         transfer.sender = event.event["transfer"]["sender"][i];
-        transfer.reciever = event.event["transfer"]["reciever"][i];
+        transfer.reciever = event.event["transfer"]["recipient"][i];
         transfer.amount = event.event["transfer"]["amount"][i];
         await transfer.save();
     }
 }
 
-export async function handleFrontierEvmCall(event: TerraContractCall): Promise<void> {
+export async function handleCall(event: TerraCall): Promise<void> {
     const idx = crypto.randomBytes(32).toString("hex");
     const rewards = new Rewards(`${event.hash}-${idx}`);
     rewards.duration = event.data.execute_msg["claim_rewards_and_optionally_unlock"]["duration"]
