6846b1ac37c6b54c5c8a1f66157ceed3e11dc5b6
Naveen V <velnaveen99@gmail.com>
2022-03-15 20:03:07 +0530

    Support changes in TerraEvent and TerraCall

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -5,7 +5,7 @@ import * as crypto from "crypto";
 export async function handleEvent(event: TerraEvent): Promise<void> {
     for(var i = 0; i < event.event["transfer"]["sender"].length; i++) {
         const idx = crypto.randomBytes(32).toString("hex");
-        const transfer = new Transfer(`${event.transactionHash}-${event.msgIndex}-${idx}`);
+        const transfer = new Transfer(`${event.txInfo.txhash}-${event.msgIndex}-${idx}`);
         transfer.sender = event.event["transfer"]["sender"][i];
         transfer.reciever = event.event["transfer"]["recipient"][i];
         transfer.amount = event.event["transfer"]["amount"][i];
@@ -15,7 +15,7 @@ export async function handleEvent(event: TerraEvent): Promise<void> {
 
 export async function handleCall(event: TerraCall): Promise<void> {
     const idx = crypto.randomBytes(32).toString("hex");
-    const rewards = new Rewards(`${event.hash}-${idx}`);
+    const rewards = new Rewards(`${event.tx.txhash}-${idx}`);
     rewards.duration = event.data.execute_msg["claim_rewards_and_optionally_unlock"]["duration"]
     rewards.withdrawLpStake = event.data.execute_msg["claim_rewards_and_optionally_unlock"]["withdraw_lp_stake"]
     rewards.terraswapLpToken = event.data.execute_msg["claim_rewards_and_optionally_unlock"]["terraswap_lp_token"]
